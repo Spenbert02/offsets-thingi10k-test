06953dcfefde99a479b17ea1878b47098770159e
Spencer Bertram <spenbert02@gmail.com>
2026-03-26 16:05:07 -0400

    hpc script

---
+++ bash_scripts/process_tetwild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=tetwild_processing
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64G
+#SBATCH --time=48:00:00
+#SBATCH --account=torch_pr_870_general
+#SBATCH --output=tetwild_output_%j.out  # This captures your print statements/progress bar
+
+module purge
+module load anaconda3/2025.06
+source /share/apps/anaconda3/2025.06/etc/profile.d/conda.sh
+export PATH_TO_ENV=$SCRATCH/conda/envs/offsets_test_thingi10k
+source activate $PATH_TO_ENV
+export LD_LIBRARY_PATH=$SCRATCH/gmp_install/lib:$LD_LIBRARY_PATH
+python -u $SCRATCH/offsets_testing_thingi10k/offsets-thingi10k-test/python_scripts/process_tetwild.py -m $SCRATCH/offsets_testing_thingi10k/tagged_tet_mshes -e $SCRATCH/TetWild/build/TetWild
