5cbaeaeeaa649051b445456ab23377a7fc3967e0
Spencer Bertram <spenbert02@gmail.com>
2026-04-05 09:30:04 -0500

    offset prog check script

---
+++ python_scripts/check_offset_successes.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+
+mesh_dir = "/scratch/seb9449/offsets_testing_thingi10k/tagged_tet_mshes"
+
+
+def main():
+    mesh_dir_path = Path(mesh_dir)
+    counts = {"success":0, "total":0}
+    for subdir in mesh_dir_path.glob("model_*"):
+        if not (subdir.exists() and subdir.is_dir()):
+            continue
+
+        try:
+            model_id = int(subdir.name.split("_")[1])
+        except:
+            print(f"WARNING: non-int model id at {str(subdir)}")
+            continue
+
+        counts["total"] += 1
+        single_out_path = subdir / "singlebody" / f"model_{model_id}_singlebody_offset_output.msh"
+        if single_out_path.exists():
+            counts["success"] += 1
+    
+    print(f"Offsets progress: {counts['success']} / {counts['total']} successes")
