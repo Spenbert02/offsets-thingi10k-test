cd3bc17022fa7feb25dfa6f12384797d922cc966
Spencer Bertram <spenbert02@gmail.com>
2026-04-18 16:59:45 -0400

    manual jobs

---
+++ bash_scripts/manual/offsets_1602764_twobody.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=offsets_1602764_2body
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32G
+#SBATCH --time=12:00:00
+#SBATCH --account=torch_pr_870_general
+#SBATCH --output=logs/job_%j_0.out 
+#SBATCH --error=logs/job_%j_0.err
+
+OFFSETS_EXE="/scratch/seb9449/wildmeshing-toolkit/build/app/wmtk_app"
+JSON_PATH="/scratch/seb9449/offsets_testing_thingi10k/tagged_tet_mshes/model_1602764/twobody/twobody_1602764_offset.json"
+
+echo "=========================================="
+echo "Task ID: 0"
+echo "Processing JSON: $JSON_PATH"
+echo "=========================================="
+
+# Execute offsets
+$OFFSETS_EXE -j $JSON_PATH
