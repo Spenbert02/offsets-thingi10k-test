2729776710d322e9d3563a293d13f795af6ccb68
Spencer Bertram <spenbert02@gmail.com>
2026-04-19 12:25:40 -0400

    manifold extraction array pipeline

---
+++ bash_scripts/manifold_array/queue_manifold_array.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+import subprocess
+
+
+mesh_dir = "/scratch/seb9449/offsets_testing_thingi10k/tagged_tet_mshes"
+run_list_fpath = "/scratch/seb9449/offsets_testing_thingi10k/offsets-thingi10k-test/bash_scripts/manifold_array/pending_jobs.txt"
+slurm_script_fpath = "/scratch/seb9449/offsets_testing_thingi10k/offsets-thingi10k-test/bash_scripts/manifold_array/manifold_submit_array.slurm"
+RERUN_ALL = False
+CHUNK_SIZE = 1000
+
+def main():
+    mesh_dir_path = Path(mesh_dir)
+    if not (mesh_dir_path.exists() and mesh_dir_path.is_dir()):
+        raise FileNotFoundError(f"{str(mesh_dir_path)} does not exist")
+
+    jsons_to_run = []
+    successes = 0
+    for subdir in mesh_dir_path.glob("model_*"):
+        if not (subdir.exists() and subdir.is_dir()):
+            continue
+
+        try:
+            model_id = int(subdir.name.split("_")[1])
+        except:
+            print(f"WARNING: non-int model id at {str(subdir)}")
+            continue
+
+        input_msh_path = subdir / "tetwild_output" / f"model_{model_id}_tetwild_output_retagged.msh"
+        if not input_msh_path.exists():
+            print(f"WARNING: no retagged tetwild output for model {model_id}")
+            continue
+
+        union_output_obj_path = subdir / "manifold_union" / f"model_{model_id}_manifold_union_output.obj"
+        if union_output_obj_path.exists() and not RERUN_ALL:
+            successes += 1
+        else:
+            union_json_path = subdir / "manifold_union" / f"manifold_union_{model_id}.json"
+            if not union_json_path.exists():
+                print(f"WARNING: manifold union json {str(union_json_path)} does not exist")
+            else:
+                jsons_to_run.append(str(union_json_path))
+        
+        subtract_output_obj_path = subdir / "manifold_subtract" / f"model_{model_id}_manifold_subtract_output.obj"
+        if subtract_output_obj_path.exists() and not RERUN_ALL:
+            successes += 1
+        else:
+            subtract_json_path = subdir / "manifold_subtract" / f"manifold_subtract_{model_id}.json"
+            if not subtract_json_path.exists():
+                print(f"WARNING: manifold subtract json {str(subtract_json_path)} does not exist")
+            else:
+                jsons_to_run.append(str(subtract_json_path))
+    print(f"{successes} models already successfully offset.")
+    run_list_path = Path(run_list_fpath)
+    
+    num_jobs = len(jsons_to_run)
+    print(f"Found {num_jobs} manifold extraction jobs to run")
+    chunks = [jsons_to_run[i:i + CHUNK_SIZE] for i in range(0, num_jobs, CHUNK_SIZE)]
+    for idx, chunk in enumerate(chunks):
+        chunk_file = run_list_path.with_name(f"pending_jobs_{idx}.txt")
+        with open(chunk_file, "w") as f:
+            for json_path in chunk:
+                f.write(f"{json_path}\n")
+        arr_max = len(chunk) - 1
+        slurm_script_path = Path(slurm_script_fpath)
+        sbatch_cmd = ["sbatch", f"--array=0-{arr_max}", str(slurm_script_path), str(chunk_file)]
+        print(f"Submitting chunk {idx+1}/{len(chunks)}: {' '.join(sbatch_cmd)}")
+        try:
+            subprocess.run(sbatch_cmd, check=True)
+            print(f"Chunk {idx+1}/{len(chunks)} successfully committed to queue")
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to submit chunk {idx}. Error: {e}")
+            break
+        except FileNotFoundError:
+            print("Error: 'sbatch' command not found. Are you running this on the HPC login node?")
+            break
+
+
+if __name__ == "__main__":
+    main()
