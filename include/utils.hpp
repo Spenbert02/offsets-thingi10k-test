088bd3fb9aa781e5fec51f9de72dc35fb7d6d522
Spencer Bertram <spenbert02@gmail.com>
2026-04-23 16:01:24 -0400

    check remeshing 1 progress

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,4 +1,9 @@
 #include <string>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+namespace fs = std::filesystem;
 
 bool get_model_id(const std::string &name, int &id)
 {
@@ -20,4 +25,40 @@ bool get_model_id(const std::string &name, int &id)
     }
     id = std::stoi(name.substr(start, end - start));
     return true;
+}
+
+bool get_max_energy_from_log(const fs::path &log, double &ret_energy)
+{
+    std::ifstream f(log.string());
+    if (!f.is_open())
+    {
+        std::string errstr = "Could not open log file " + log.string();
+        throw std::runtime_error(errstr);
+    }
+
+    std::string line;
+    while (std::getline(f, line))
+    {
+        if (line.empty())
+            continue;
+        size_t colon_pos = line.find(":");
+        if (colon_pos == std::string::npos)
+            continue;
+        std::string key = line.substr(0, colon_pos);
+        key.erase(std::remove_if(key.begin(), key.end(), ::isspace), key.end());
+        if (key == "max_energy")
+        {
+            std::string value_str = line.substr(colon_pos + 1);
+            try
+            {
+                ret_energy = std::stod(value_str);
+                return true;
+            }
+            catch (const std::exception &e)
+            {
+                continue;
+            }
+        }
+    }
+    return false;
 }
