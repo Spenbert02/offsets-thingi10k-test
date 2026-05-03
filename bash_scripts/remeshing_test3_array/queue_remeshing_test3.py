ed2e80e1b8cdf29e09c344b07280597d1eec1e02
Spencer Bertram <spenbert02@gmail.com>
2026-05-03 15:12:47 -0400

    chunking

--- bash_scripts/remeshing_test3_array/queue_remeshing_test3.py
+++ bash_scripts/remeshing_test3_array/queue_remeshing_test3.py
@@ -6,8 +6,8 @@ mesh_dir = "/scratch/seb9449/offsets_testing_thingi10k/tagged_tet_mshes"
 run_list_fpath = "/scratch/seb9449/offsets_testing_thingi10k/offsets-thingi10k-test/bash_scripts/remeshing_test3_array/pending_jobs.txt"
 slurm_script_fpath = "/scratch/seb9449/offsets_testing_thingi10k/offsets-thingi10k-test/bash_scripts/remeshing_test3_array/remeshing_test3_submit_array.slurm"
 RERUN_ALL = False
-CHUNK_SIZE = 10
-MAX_CHUNKS = 1
+CHUNK_SIZE = 1000
+MAX_CHUNKS = None
 
 def main():
     mesh_dir_path = Path(mesh_dir)
