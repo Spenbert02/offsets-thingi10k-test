80f1ffd0ff6959a7e318eaba07e3f55faf6a7c8b
Spencer Bertram <spenbert02@gmail.com>
2026-06-05 13:54:27 -0400

    test 6

---
+++ python_scripts/create_remeshing6_jsons.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import json
+
+msh_dir_path = Path("/scratch/seb9449/offsets_testing_thingi10k/tagged_tet_mshes")
+
+def main():
+    if not msh_dir_path.exists():
+        raise FileNotFoundError(str(msh_dir_path))
+
+    count = 0
+    for subdir in msh_dir_path.glob("model_*"):
+        if subdir.exists() and subdir.is_dir():
+            try:
+                model_id = int(subdir.name.split("_")[1])
+            except:
+                print(f"WARNING: non int dir name: {subdir.name}")
+                continue
+
+            obj_path = subdir / f"model_{model_id}.obj"
+            if not obj_path.exists():
+                print(f"WARNING: obj for model {model_id} does not exist")
+
+            output_dir = subdir / "remeshing_test6"
+            output_dir.mkdir(parents=True, exist_ok=True)
+            json_path = output_dir / f"remeshing_test6_{model_id}.json"
+            json_data = {
+                "application": "image_simulation",
+                "input": [str(obj_path)],
+                "output": f"model_{model_id}_out",
+                "skip_simplify": False,
+                "preserve_topology": False,
+                "num_threads": 0,
+                "write_envelope": False,
+                "DEBUG_output": False,
+                "log_file": f"model_{model_id}_out"
+            }
+            with open(json_path, "w") as f:
+                json.dump(json_data, f, indent=4)
+
+            count += 1
+    print(f"Created {count} jsons.")
+
+
+if __name__ == "__main__":
+    main()
